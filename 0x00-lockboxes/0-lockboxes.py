1a4acec3d95f6296b249024a7a8807a4da178ea9
Diego Gómez <dagomez2530@gmail.com>
2020-12-10 13:42:05 -0500

    Adding task 0 lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,45 +7,27 @@ def canUnlockAll(boxes):
     Description: determines if all the boxes can be opened.
     boxes: is a list of lists
     returns: True in case that all boxes can be opened, False otherwise
-    """
+    # """
+    unlock = []
+    keys = []
     lg = len(boxes)
-    visitados = []
-    i = 0
-    while i < lg:
-        visitados.append(0)
-        i += 1
-    j = 0
-    if lg != 0:
-        visitados[0] = 1
-        lg0 = len(boxes[0])
-    j = 0
-    while j < lg0:
-        op, v = unlock(boxes, visitados, boxes[0][j], 0)
-        if op and 0 not in v:
-            return True
-        j += 1
-    return False
+    for i in range(lg):
+        unlock.append(0)
+    unlock[0] = 1
+    for j in range(len(boxes[0])):
+        keys.append(boxes[0][j])
 
+    for k in keys:
+        # print("key:", k, "| keys:", keys, "| unlocked", unlock)
+        if k < lg and unlock[k] == 0:
+            unlock[k] = 1
+        if k >= lg:
+            continue
+        for nk in boxes[k]:
+            if nk not in keys:
+                keys.append(nk)
 
-def unlock(boxes, visitados, index, prev_idx):
-    """
-    Description: helper function for unlock boxes
-    boxes: is a list of lists
-    visitados: 0 not visited, other numbers for other cases
-    index: index of current box
-    prev_idx: index of the previous box
-    returns: A tuple with (open, visitados)
-    """
-    if visitados[prev_idx] > len(boxes[prev_idx]) and visitados[index] > 1:
-        return False, visitados
-
-    visitados[index] += 1
-    lg_index = len(boxes[index])
-    i = 0
-    while i < lg_index:
-        # print("i value", i)
-        op, v = unlock(boxes, visitados, boxes[index][i], index)
-        if 0 not in v:
-            return True, visitados
-        i += 1
-    return False, visitados
+    if 0 in unlock:
+        return False
+    else:
+        return True
