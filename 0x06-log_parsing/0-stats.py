a2ddfcc7566a36af6c6984a33e535d79f291a54a
Diego Gómez <dagomez2530@gmail.com>
2021-01-27 12:08:11 -0500

    Adding file for task 0

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -17,21 +17,22 @@ try:
         # date = re.findall(r"\d+-\d+-\d+ [\d.:]+", line)
         # get = re.findall(r"GET [/.\d\w\s]+", line)
         line_split = line.split()
-        status = line_split[-2]
-        size = line_split[-1]
-        sz += int(size)
-        # count status
-        if status in count_status:
-            count_status[status] += 1
-        # print("line split", line_split)
+        if len(line_split) > 2:
+            status = line_split[-2]
+            size = line_split[-1]
+            sz += int(size)
+            # count status
+            if status in count_status:
+                count_status[status] += 1
+            # print("line split", line_split)
 
-        # print("MATCHES", ip, date, get, status, size)
-        cont += 1
-        if cont % 10 == 0:
-            print("File size: {}".format(sz))
-            for k, v in count_status.items():
-                if v != 0:
-                    print("{}: {}".format(k, v))
+            # print("MATCHES", ip, date, get, status, size)
+            cont += 1
+            if cont % 10 == 0:
+                print("File size: {}".format(sz))
+                for k, v in count_status.items():
+                    if v != 0:
+                        print("{}: {}".format(k, v))
 finally:
     print("File size: {}".format(sz))
     for k, v in count_status.items():
