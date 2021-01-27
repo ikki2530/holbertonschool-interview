743f73169a1f30f27021e872bcd5f60904d62704
Diego Gómez <dagomez2530@gmail.com>
2021-01-27 11:55:17 -0500

    Adding file for task 0

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Reads stdin line by line and computes metrics
+"""
+import sys
+import re
+
+
+cont = 0
+sz = 0
+count_status = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+
+try:
+    for line in sys.stdin:
+        # print("$", line)
+        ip = re.findall(r"\d+\.\d+\.\d+\.\d+", line)
+        date = re.findall(r"\d+-\d+-\d+ [\d.:]+", line)
+        get = re.findall(r"GET [/.\d\w\s]+", line)
+        line_split = line.split()
+        status = line_split[-2]
+        size = line_split[-1]
+        sz += int(size)
+        # count status
+        if status in count_status:
+            count_status[status] += 1
+        # print("line split", line_split)
+
+        # print("MATCHES", ip, date, get, status, size)
+        cont += 1
+        if cont % 10 == 0:
+            print("File size: {}".format(sz))
+            for k, v in count_status.items():
+                if v != 0:
+                    print("{}: {}".format(k, v))
+except KeyboardInterrupt:
+    print("File size: {}".format(sz))
+    for k, v in count_status.items():
+        if v != 0:
+            print("{}: {}".format(k, v))
